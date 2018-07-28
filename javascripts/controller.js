70f07ee8a441edee88a560b444c7b8930ea28183
Ram ( Ramudu Kondapalli) <ramudu.kp@gmail.com>
2018-07-27 23:39:33 -0400

    Add files via upload

--- javascripts/controller.js
+++ javascripts/controller.js
@@ -1,11 +1,19 @@
 var TaskController = function() {
 	var controller = this;
-	controller.addNewTask = function() {
-		controller.tasks.push(controller.newTask);
-		controller.newTask = '';
+	controller.isValidNewTask = function() {
+		return controller.newTask.trim() !== '';
 	}
-	controller.tasks = [ 'Task 1', 'Task 2', 'Task 3', 'Task 4' ];
+	controller.addNewTask = function() {
+		controller.updateMessage = '';
+		if (controller.isValidNewTask()) {
+			controller.tasks.push(controller.newTask);
+			controller.newTask = '';
+			controller.updateMessage = 'New Task Added !!! ';
+		}
+	}
+	controller.tasks = [ 'Task 1', 'Task 2', 'Task 3' ];
 	controller.newTask = '';
+	controller.updateMessage = '';
 }
 
 angular.module('MyApp', []).controller('TaskController', TaskController);
