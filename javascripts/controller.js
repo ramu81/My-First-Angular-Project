4079f7093559b1c49879328482f5cb2dea148029
Ram ( Ramudu Kondapalli) <ramudu.kp@gmail.com>
2018-08-02 23:47:16 -0400

    update
    updated

--- javascripts/controller.js
+++ javascripts/controller.js
@@ -1,19 +1,31 @@
-var TaskController = function() {
+var TaskController = function($timeout) {
 	var controller = this;
+
 	controller.isValidNewTask = function() {
 		return controller.newTask.trim() !== '';
 	}
+
 	controller.addNewTask = function() {
 		controller.updateMessage = '';
 		if (controller.isValidNewTask()) {
 			controller.tasks.push(controller.newTask);
 			controller.newTask = '';
 			controller.updateMessage = 'New Task Added !!! ';
+			var clearUpdateMsg = function() {
+				controller.updateMessage = '';
+			}
+
+			if (controller.updateMessagePromise)
+				$timeout.cancel(controller.updateMessagePromise);
+
+			controller.updateMessagePromise = $timeout(clearUpdateMsg, 5000);
 		}
 	}
 	controller.tasks = [ 'Task 1', 'Task 2', 'Task 3' ];
 	controller.newTask = '';
 	controller.updateMessage = '';
+	controller.updateMessagePromise;
+
 }
 
 angular.module('MyApp', []).controller('TaskController', TaskController);
