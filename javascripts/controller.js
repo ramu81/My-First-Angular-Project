eed8b54724620312ff57f8b11a0abea0ceec2e44
Ram ( Ramudu Kondapalli) <ramudu.kp@gmail.com>
2018-08-05 08:45:08 -0400

    Add files via upload

--- javascripts/controller.js
+++ javascripts/controller.js
@@ -1,31 +1,56 @@
-var TaskController = function($timeout) {
+var TaskController = function($filter) {
 	var controller = this;
-
-	controller.isValidNewTask = function() {
-		return controller.newTask.trim() !== '';
+	var createEmptyTask = function() {
+		return {
+			name : ' ',
+			month : 01,
+			day : 01,
+			year : 2016
+		};
 	}
-
-	controller.addNewTask = function() {
-		controller.updateMessage = '';
-		if (controller.isValidNewTask()) {
-			controller.tasks.push(controller.newTask);
-			controller.newTask = '';
-			controller.updateMessage = 'New Task Added !!! ';
-			var clearUpdateMsg = function() {
-				controller.updateMessage = '';
-			}
-
-			if (controller.updateMessagePromise)
-				$timeout.cancel(controller.updateMessagePromise);
-
-			controller.updateMessagePromise = $timeout(clearUpdateMsg, 5000);
+	controller.tasks = [ {
+		name : 'Task A',
+		month : 01,
+		day : 06,
+		year : 2016
+	}, {
+		name : 'Task A',
+		month : 15,
+		day : 12,
+		year : 2015
+	}, {
+		name : 'Task B',
+		month : 01,
+		day : 06,
+		year : 2016
+	}, {
+		name : 'Task B',
+		month : 15,
+		day : 12,
+		year : 2015
+	} ];
+	controller.getTasks = function() {
+		var orderBy = $filter('orderBy');
+		return orderBy(controller.tasks, '[year,month,day,name]', false);
+	}
+	controller.addTask = function() {
+		controller.tasks.push(controller.newTask);
+		controller.newTask = createEmptyTask();
+	}
+	controller.deleteTask = function(task) {
+		var index = controller.tasks.indexOf(task);
+		if (index > -1) {
+			controller.tasks.splice(index,1);
 		}
 	}
-	controller.tasks = [ 'Task 1', 'Task 2', 'Task 3' ];
-	controller.newTask = '';
-	controller.updateMessage = '';
-	controller.updateMessagePromise;
-
+	
+	controller.newTask = createEmptyTask();
+	
+	controller.disableAddTask = function() {
+		return controller.newTask.name.trim() === '';
+	}
+	
 }
 
-angular.module('MyApp', []).controller('TaskController', TaskController);
+angular.module('todoApp', [])
+	   .controller('TaskController', TaskController);
